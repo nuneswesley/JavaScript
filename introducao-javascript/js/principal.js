c39c46a1c75d0f877932392ca82da1fbaaf4c4c8
Wesley Nunes <nunes.wesley@hotmail.com>
2017-09-28 10:41:26 -0300

    Alteracoes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -35,10 +35,65 @@ for(var i = 0 ; i < pacientes.length; i++){
     }
 }
 
-
-
-
 // console.log(paciente); //tr
 // console.log(peso);  
 // console.log(altura);  
 // console.log(imc);
+
+// titulo.addEventListener("click", mostraMensagem);
+
+// function mostraMensagem(){
+//     console.log("Olá eu fui clicado!");
+// }
+
+
+// titulo.addEventListener("click", function(){
+//     console.log("Ola função anonima");
+// } )
+
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    tabela.appendChild(pacienteTr);
+
+
+    console.log(pacienteTr);
+    
+
+    // console.log(form.nome.value);
+    // console.log(form.peso.value);
+    // console.log(form.altura.value);
+    // console.log(form.gordura.value);
+
+})
+// console.log(botaoAdicionar);
