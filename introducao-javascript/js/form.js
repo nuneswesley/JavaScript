3f6a2ca680922f515525d1344494a02d51aacc11
Wesley Nunes <nunes.wesley@hotmail.com>
2017-10-02 23:33:48 -0300

    Aula 6 (Finalizada)

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -9,11 +9,10 @@ botaoAdicionar.addEventListener("click", function(){
    
     var pacienteTr = montaTr(paciente);
     
-    var erro = validaPaciente(paciente);
+    var erros = validaPaciente(paciente);
 
-    if(erro.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros)
         return;
     }
 
@@ -22,7 +21,21 @@ botaoAdicionar.addEventListener("click", function(){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
 })
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form) {
     var paciente = {
@@ -61,11 +74,24 @@ function validaPaciente(paciente){
 
     var erros = [];
 
+    if (paciente.nome.length ==0)
+        erros.push("O nome não pode ser em branco!");
+
     if(!validaPeso(paciente.peso)) 
-        erros.push("Peso é inválido");  
+        erros.push("Peso é inválido!");  
 
     if(!validaAltura(paciente.altura)) 
-        erros.push("Altura é inválida");
+        erros.push("Altura é inválida!");
     
-    return erros;
+    if(paciente.gordura.length == 0)
+        erros.push("A gordura não pode ser em branco!");
+
+    if(paciente.peso.length == 0)
+        erros.push("O peso não pode ser em branco!");
+    
+    if(paciente.altura.length == 0)
+        erros.push("A altura não pode ser em branco!");
+    
+return erros;
+
 }
